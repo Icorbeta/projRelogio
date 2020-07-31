0f1bae338587cf77d44c834dca8754ac06d6991d
IVAN DONISETE CORBETA <005699988@itaud.des.ihf>
2020-07-31 16:43:17 -0300

    adicionei novo atributo

---
+++ Relogio.java
@@ -0,0 +1,23 @@
+import jdk.vm.ci.code.site.Site;
+
+/**
+ * Relogio
+ */
+public class Relogio {
+
+    private String hora;
+
+    public String getHora();{
+
+        private String minutos;
+
+        public String getMinutos();{
+
+            private String segundos;
+
+            public String getSegundos();{
+
+            }
+        }
+    }
+}
